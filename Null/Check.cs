27e5e44d1f73c5c72628c1feea4abb10dcfedbfd
new_user <new_user@gmail.com>
2021-09-06 14:35:16 +0300

    Добавьте файлы проекта.

---
+++ Null/Check.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Null
+{
+    class Check
+    {
+        public static void IsNull(Action action, params object[] obj)
+        {
+            lock (obj)
+            {
+                if (check == null) check = new Check();
+            }
+            check._IsNull(action, obj);
+        }
+
+        [ThreadStatic]
+        static Check check;
+        static object obj;
+        static Check()
+        {
+            obj = new object();
+        }
+        int number;
+        private void _IsNull(Action action, object[] obj)
+        {
+            Console.WriteLine(number);
+            foreach (var item in obj)
+                if (item is null) action();
+        }
+        public Check()
+        {
+            this.number = new Random().Next(10, 100);
+        }
+    }
+}
---
+++ Null/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace Null
+{
+    class Program
+    {
+        static void Start()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Check.IsNull(() => Console.WriteLine("isNull"), "", "", null);
+            }
+        }
+        static void Main(string[] args)
+        {
+            new Thread(Start).Start();
+            new Thread(() => Check.IsNull(() => Console.WriteLine("isNull"), "", "", null)).Start();
+            new Thread(() => Check.IsNull(() => Console.WriteLine("isNull"), "", "", null)).Start();
+        }
+    }
+}
